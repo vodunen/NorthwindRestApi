481f21f9f839aa666c5fe4e8d1a9657bafacffb0
vodunen <danielbavenmark@gmail.com>
2026-02-19 14:57:52 +0400

    Lisäystoiminto

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -15,8 +15,57 @@ namespace NorthwindRestApi.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Tapahtui virhe. Lue lisää: {e.InnerException?.Message}");
+            }
+        }
+
+        // Hakee yhen asiakkaan pääavaimella
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    //return BadRequest("Asiakasta id:llä " + id + " ei löydy.");
+                    return NotFound($"Asiakasta id:llä {id} ei löydy."); //string interpolation
+                }
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää:" + e);
+
+            }
+
+        }
+
+        // Uuden lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust)
+        {
+            try
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi asiakas " + {cust.CompanyName} from {cust.City}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException:);
+            }
         }
 
     }
