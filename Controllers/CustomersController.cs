320873303a50d220f238c69297ec33c57253d0ca
vodunen <danielbavenmark@gmail.com>
2026-02-19 13:31:59 +0400

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models;
+
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // Alustetaan tietokantayhteys
+        NorthwindOriginalContext db = new NorthwindOriginalContext();
+
+        // Hakee kaikki asiakkaat
+        [HttpGet]
+        public ActionResult GetAllCustomers()
+        {
+            var asiakkaat = db.Customers.ToList();
+            return Ok(asiakkaat);
+        }
+
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthwindRestApi.Models;
+
+public partial class Login
+{
+    public int LoginId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string PassWord { get; set; } = null!;
+
+    public string? LoginErrorMessage { get; set; }
+
+    public bool Maintenance { get; set; }
+
+    public DateOnly? RegisterationDate { get; set; }
+
+    public string? Salt { get; set; }
+}
